0a3aefa0b356362d5b5e26f33ca43da7d6e6d94b
webermal <webermal@hu-berlin.de>
2021-03-10 21:27:00 +0100

    Several Changes on Improve-part

--- networkit/cpp/community/ImproveClustering.cpp
+++ networkit/cpp/community/ImproveClustering.cpp
@@ -6,26 +6,40 @@
 #include <networkit/graph/Graph.hpp>
 #include <networkit/community/MinCutStoerWagner.hpp>
 #include <iostream>
-
+#include <networkit/community/GraphClusteringTools.hpp>
+#include <networkit/community/EdgeCut.hpp>
 
 namespace NetworKit {
 
     ImproveClustering::ImproveClustering(const Graph &G, const Partition initPartition):
-      CommunityDetectionAlgorithm(G), iPartition(initPartition.getVector()), flowGraph(G) {
+      CommunityDetectionAlgorithm(G, initPartition), flowGraph(G) {
 
         // add source (s) and sink (t) to the flowGraph
         s = flowGraph.addNode();
         t = flowGraph.addNode();
 
-        // TODO check if initPartition has exactely two Groups
-        iPartition.compact();
-        minS = iPartition.getMembers(0).size() < iPartition.getMembers(1).size() ? 0 : 1;
+        // Check if initial Partition has 2 Clusters
+        if(result.subsetSizes().size() != 2){
+            throw std::runtime_error("The number of Clusters in the initial Partition must be 2");
+        }
 
-        f_a = (double)iPartition.getMembers(minS).size()/
-              (double)(G.numberOfNodes() - iPartition.getMembers(minS).size());
+        //Check if initial Partition is a proper Clustering
+        if(!GraphClusteringTools::isProperClustering(G, result)){
+            throw std::runtime_error("The initial Partition must be a proper clustering");
+        }
 
+        // Compute the set of indices A from the initial Partition
+        result.compact();
+        index minS = result.getMembers(0).size() < result.getMembers(1).size() ? 0 : 1;
+        A = result.getMembers(minS);
+
+        // Compute f_a the ration between A and the complement of A
+        f_a = (double)A.size()/
+              (double)(G.numberOfNodes() - A.size());
+
+        // Add edges to the transformed Graph "flowGraph" from s to A and from complement(A) to t
         G.forNodes([&](const node n){
-            if (iPartition.subsetOf(n) == minS){
+            if (A.find(n) != A.end()){
                 flowGraph.addEdge(s, n, 1);
             } else {
                 flowGraph.addEdge(n, t, 1);
@@ -38,8 +52,8 @@ namespace NetworKit {
         INFO("FLOW GRAPH deg s", flowGraph.degree(s));
         INFO("FLOW GRAPH deg t", flowGraph.degree(t));
 
-        INFO("Init Partition", iPartition.getVector());
-        double alpha_0 = relativeQuotientScore(G, iPartition.getMembers(minS), iPartition.getMembers(minS));
+        INFO("Init Partition", result.getVector());
+        double alpha_0 = relativeQuotientScore(result);
         double alpha;
         int i = 0;
         do {
@@ -57,20 +71,22 @@ namespace NetworKit {
 
             INFO("MIN CUT IN IMPROVE RUN: ", minCutPartition.getVector());
 
-            std::set<index> SetS(minCutPartition.getMembers(minCutPartition.subsetOf(s)));
-            SetS.erase(s);
-            alpha_0 = relativeQuotientScore(
-                G,
-                iPartition.getMembers(minS),
-                SetS
-                );
+
+
+            alpha_0 = relativeQuotientScore(minCutPartition);
+
             INFO("ALPHA 0: ", alpha_0);
+
+            /*
             // Computing result partition
             // TODO Do this better
             std::vector<index> resultVec;
             // TODO Problem if the node are not enumerated from 0 to one
+
+
             resultVec.resize(flowGraph.upperNodeIdBound());
             minCutPartition.compact();
+
 
             std::set<index> sourceSet(minCutPartition.getMembers(minCutPartition.subsetOf(s)));
             std::set<index> sinkSet(minCutPartition.getMembers(minCutPartition.subsetOf(t)));
@@ -86,31 +102,28 @@ namespace NetworKit {
                 resultVec[i] = 1;
             }
             result = Partition(resultVec);
+            */
 
         } while (i < 10);
 
         hasRun = true;
     }
 
-    double ImproveClustering::relativeQuotientScore(const Graph* G, std::set<node> A, std::set<node> S){
-        //double f = (double)A.size() / (double )(G->numberOfNodes() - A.size());
-        double d = 0.0;
+    double ImproveClustering::relativeQuotientScore(const Partition& p){
 
-        // TODO not so ugly!!!!
+        S = p.getMembers(p.subsetOf(s));
 
-        INFO("SET A", A);
-        INFO("SET S", S);
+        S.erase(s);
+        S.erase(t);
+
+        double dAOfS = 0.0;
         for (const node n: S){
-            if (n != s and n != t){
-                d += A.find(n) != A.end() ? 1.0 : -f_a;
-                INFO("DDDDDD: ",d );
-            }
-
+            dAOfS += A.find(n) != A.end() ? 1.0 : -f_a;
         }
-        INFO("d hat den Wert: ", d);
 
+        INFO("d hat den Wert: ", dAOfS);
+/*
         double deltaS = 0.0;
-        // TODO use Edgcut get Quality
         flowGraph.forEdges([&](const node u, const node v, const edgeweight w){
             if (u != t and v != t and u != s and v != s){
                 if ((S.count(u) and !S.count(v)) or (!S.count(u) and S.count(v))){
@@ -119,17 +132,20 @@ namespace NetworKit {
                 }
             }
         });
+       */
 
-        flowGraph.forEdges([&](const node u, const node v, const edgeweight w){
-            INFO(" EDGE: ", u, " TO " ,v , " WITH WEIGHT ", w );
-        });
+        // Compute the Cut-Value of S / complement(S)
+        EdgeCut ec;
+        double boundaryS = ec.getQuality(p, *G);
 
-        INFO("DELTA S: ", deltaS);
-        INFO("D: ", d);
-        double q = d > 0.0 ? deltaS/d : std::numeric_limits<double>::max();
+        INFO("DELTA S: ", boundaryS);
+        INFO("D: ", dAOfS);
+        double q = dAOfS > 0.0 ? boundaryS/dAOfS : std::numeric_limits<double>::max();
 
         return q;
     }
+
+
 
     void ImproveClustering::updateEdgeWeights(double alpha){
         flowGraph.forNeighborsOf(s, [&](node n){
